06801d0d7908d8958c363c515c32e4056769db9d
Atapkel Beibarys <beibarysatapkel@gmail.com>
2025-04-04 08:51:31 +0500

    works

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -140,13 +140,11 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = 'minioadmin'
 AWS_SECRET_ACCESS_KEY = 'minioadmin'
-AWS_STORAGE_BUCKET_NAME = 'media'  # Name your bucket accordingly
+AWS_STORAGE_BUCKET_NAME = 'media'
 
-# Point to your local MinIO instance
 AWS_S3_ENDPOINT_URL = 'http://localhost:9000'
 AWS_S3_REGION_NAME = 'us-east-1'
-AWS_S3_USE_SSL = False  # Use SSL only if configured accordingly
-AWS_S3_VERIFY = False   # Disable SSL certificate verification for local testing
+AWS_S3_USE_SSL = False
+AWS_S3_VERIFY = False
 
-# Optionally, update your MEDIA_URL if needed
 MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/'
--- blog/entrypoint.sh
+++ blog/entrypoint.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-# Wait for PostgreSQL using psql
 until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
   echo "PostgreSQL is unavailable - sleeping"
   sleep 1
